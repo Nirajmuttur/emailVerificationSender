19dd61d4ebf3107e264ed3017e6e5e86e1058350
nirajmuttur <nirajmuttur@gmail.com>
2024-06-02 12:12:53 +0530

    removed api key

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
 
   const { userId, secret } = req.query
   if (!userId || !secret) {
